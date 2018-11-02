f7db9c61dd094ba09a36bb3c0aa55e92d5e4a53f
Ryan McGee <ramtech51@comcast.net>
2018-11-01 23:51:34 -0400

    Fixed unused imports

--- src/window/MobileController.java
+++ src/window/MobileController.java
@@ -1,15 +1,11 @@
 package window;
 
-import java.awt.EventQueue;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Properties;
 
 public class MobileController
 {
